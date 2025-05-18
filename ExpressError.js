9c622647d50ac627ba8df2ae1aa975be9877e63c
Sudip Khatiwada <ksudip121@gmail.com>
2025-05-18 11:24:21 +0530

    handling errors

---
+++ ExpressError.js
@@ -0,0 +1,11 @@
+//this file is created after learning Error handilng
+
+class ExpressError extends Error {
+    constructor(status, message) {
+        super();
+        this.status = status;
+        this.message = message;
+    }
+}
+
+module.exports = ExpressError;
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose")
 const path = require("path")
 const Chat = require("./models/chat.js")
 const methodOverride = require("method-override")
+const ExpressError = require("./ExpressError.js") //after learning middleware
+const wrapAsync = require("./utils/wrapAsync.js")
 
 app.set("views" , path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -24,11 +26,11 @@ async function main() {
 }
 
 //Index Route
-app.get("/chats" , async(req, res) => {
+app.get("/chats" , wrapAsync(async(req, res) => {
     let chats =  await Chat.find()
     // console.log(chats);
     res.render("index.ejs", {chats})
-})
+}))
 
 //New chat Route
 app.get("/chats/new" , (req, res) => {
@@ -52,15 +54,25 @@ app.post("/chats" , (req, res) => {
     res.redirect("/chats")
 })
 
+//New- Show Route (After Learning Middleware error)
+app.get("/chats/:id" , wrapAsync(async(req, res, next) => {
+    let {id} = req.params;
+    let chat = await Chat.findById(id);
+    if (!chat) {
+        return next(new ExpressError(404, "chat not found"))
+    }
+    res.render("edit.ejs" , {chat})
+}))
+
 //Edit Route
-app.get("/chats/:id/edit" , async(req, res) => {
+app.get("/chats/:id/edit" , wrapAsync(async(req, res) => {
     let {id} = req.params;
     let chat =  await Chat.findById(id)
 res.render("edit.ejs", {chat})
-})
+}))
 
 //Update Route
-app.put("/chats/:id" , async(req, res) => {
+app.put("/chats/:id" , wrapAsync(async(req, res) => {
     let {id} = req.params;
     let {message : newMessage} = req.body;
     let updatedChat = await Chat.findByIdAndUpdate(id, 
@@ -68,18 +80,24 @@ app.put("/chats/:id" , async(req, res) => {
         {runValidators: true, new: true}
     )
     res.redirect("/chats")
-})
+}))
 
 //DELETE Route
-app.delete("/chats/:id" , async(req, res) => {
+app.delete("/chats/:id" , wrapAsync(async(req, res) => {
     let {id} = req.params;
     let deletedChat = await Chat.findByIdAndDelete(id)
     console.log(deletedChat)
     res.redirect("/chats")
-})
+}))
 
 app.get("/", (req, res) => {
     res.send("working")
+})
+
+//Error handiling route or Middleware
+app.use((err, req, res, next) => {
+    let {status=500, message="SOME ERROR OCCURED"} = err;
+    res.status(status).send(message);
 })
 
 app.listen(8080, () => {
